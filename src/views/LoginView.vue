51b2e9451af0f84652a509e0355c879c457009fb
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2025-05-08 00:10:22 -0300

    fix loginview y se coloca fondo light

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row class="background">
+  <v-row :class="backgroundClass">
     <v-col md="12" sm="6">
       <SingIn v-if="suForm" />
     </v-col>
@@ -7,21 +7,20 @@
 </template>
 
 <script lang="ts" setup>
-import { Ref, ref, onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue'
+import type { Ref } from 'vue'
 import { useTheme } from 'vuetify'
 import SingIn from '../components/auth/SingIn.vue'
 
 const theme = useTheme()
 const suForm: Ref<boolean> = ref(true)
 
-// Función para alternar el tema y guardarlo en localStorage
-// function toggleTheme() {
-//   const newTheme = theme.global.current.value.dark ? 'light' : 'dark'
-//   theme.global.name.value = newTheme
-//   localStorage.setItem('theme', newTheme) // Guardar en localStorage
-// }
+// Clase dinámica según el tema
+const backgroundClass = computed(() =>
+  theme.global.current.value.dark ? 'background-dark' : 'background-light',
+)
 
-// Al montar el componente, recuperar el tema de localStorage
+// Recuperar tema guardado
 onMounted(() => {
   const savedTheme = localStorage.getItem('theme')
   if (savedTheme) {
@@ -31,9 +30,8 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.background {
+.background-dark {
   margin: 0;
-  background: rgb(107, 89, 214);
   background: radial-gradient(
     circle at 50% 100%,
     rgba(107, 89, 214, 1) 0%,
@@ -42,10 +40,26 @@ onMounted(() => {
     rgba(54, 47, 94, 1) 73%,
     rgba(40, 36, 61, 1) 100%
   );
-  min-height: 100vh; /* Altura mínima del viewport */
-  width: 100vw; /* Ancho completo */
+  min-height: 100vh;
+  width: 100vw;
   display: flex;
-  justify-content: center; /* Centrado horizontal */
-  align-items: center; /* Centrado vertical */
+  justify-content: center;
+  align-items: center;
+}
+
+.background-light {
+  margin: 0;
+  background: radial-gradient(
+    circle at 50% 100%,
+    #eeeeee 0%,
+    #cccccc 30%,
+    #bbbbbb 60%,
+    #999999 100%
+  );
+  min-height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
